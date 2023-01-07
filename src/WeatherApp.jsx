ac7dca10fa0c296eccaf14d798adf4e37fe457e1
Lafsitoo <fretteleandro@live.com>
2023-01-07 04:10:57 -0300

    07/01-04:10

--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import "./WeatherApp.css";
 
 function WeatherApp() {
-  return <div className="App"></div>;
+  return <div className="App">Hola</div>;
 }
 
 export default App;
