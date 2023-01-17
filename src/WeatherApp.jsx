e9eedcf787b53993b866b3b9ec02794cca915c38
Lafsitoo <fretteleandro@live.com>
2023-01-17 19:27:00 -0300

    17/01-19:27

--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import "./WeatherApp.css";
 
 function WeatherApp() {
+  // LOGICA
+  const [weather, setWeatger] = useState(null)
+  
+
+
+  // RENDER
   return <div className="App">Weather</div>;
 }
 
